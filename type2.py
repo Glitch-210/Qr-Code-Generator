b887a2fa3d4e1261bba7353e0fb27f1909382b1d
Mustafa <mustafakhericha89@gmail.com>
2024-11-11 13:04:50 +0530

    customize

--- type2.py
+++ type2.py
@@ -1,1 +1,9 @@
-type2.py
+import qrcode as qr
+from PIL import Image
+
+qr1 = qr.QRCode(version=1,error_correction=qr.constants.ERROR_CORRECT_H,box_size=10,border=6)
+qr1.add_data("https://github.com/Glitch-210/Qr-Code-Generator")
+qr1.make(fit=True)
+
+img = qr1.make_image(fill_color="black",back_color="white")
+img.save("name11.png")
